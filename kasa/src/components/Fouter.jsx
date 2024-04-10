84a1467a201f00b5180de14603abb66ae2296a80
Dammiieenn <157717253+Dammiieenn@users.noreply.github.com>
2024-04-10 11:02:25 +0200

    ulr manquant

--- kasa/src/components/Fouter.jsx
+++ kasa/src/components/Fouter.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import "../styles/fouter.css";
 
 const Fouter = () => {
     return (
         <div className='fouter'>
+            <div className='logo2'>
             <img src='./logo2.png' alt='logo react'/>
+            </div>
             <h3>2020 Kasa. All rights reserved</h3>
             
         </div>
