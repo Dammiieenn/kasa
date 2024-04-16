b0f1ce345de0df735147525f45baefe702bc6e41
Dammiieenn <157717253+Dammiieenn@users.noreply.github.com>
2024-04-16 16:01:22 +0200

    probleme css

--- kasa/src/components/Logement.jsx
+++ kasa/src/components/Logement.jsx
@@ -1,34 +1,30 @@
 import React from "react";
 import UseUtilitaire from "../UseUtilitaire";
+import "../styles/logement.css";
+import image from "../image/image1.png";
 
-  export default function Logement () {
-    const { data, loading, error } = UseUtilitaire("./logements.json");
-  
-    if (loading) return <div>Loading ...</div>;
-    if (error) return <div>Error: {error}</div>;
-  
-    console.log(data);
-    
-      return (
-        <div>
-          <h1>Liste des logements</h1>
-          <ul>
-            {data.map((logement, index) => (
-              <li key={index}>
-                <h2>{logement.title}</h2>
-                <img src={logement.cover} alt={logement.title} />
-                <h3>Photos</h3>
-                <ul>
-                  {logement.pictures.map((picture, pictureIndex) => (
-                    <li key={pictureIndex}>
-                      <img src={picture} alt={`Image ${pictureIndex + 1}`} />
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            ))}
-          </ul>
-        </div>
-      );
-  }
-  
+export default function Logement() {
+  const { data, loading, error } = UseUtilitaire("./logements.json");
+
+  if (loading) return <div>Loading ...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  console.log(data);
+
+  return (
+    <div>
+      <img src={image} alt="Premiere image" className="premiereImage" />
+      <div className="imageLogement">
+        <ul>
+          {data.map((logement, index) => (
+            <li key={index}>
+              <h2>{logement.title}</h2>
+              <img src={logement.cover} alt={logement.title} className="imageLogement" />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
